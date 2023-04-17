41d5467cf5b7cdb1129980abee6aad705e501e6a
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-04-17 00:50:58 -0400

    deepgram with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+
+  var [words, setWords] = useState([]);
+  
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    console.log({ stream })
+    if (!MediaRecorder.isTypeSupported('audio/webm'))
+      return alert('Browser not supported')
+    const mediaRecorder = new MediaRecorder(stream, {
+      mimeType: 'audio/webm',
+    })
+    const socket = new WebSocket('wss://api.deepgram.com/v1/listen', [
+      'token',
+      process.env.REACT_APP_DEEPGRAM_API_KEY,
+    ])
+    socket.onopen = () => {
+      console.log("Established connection")
+      console.log({ event: 'onopen' })
+      mediaRecorder.addEventListener('dataavailable', async (event) => {
+        if (event.data.size > 0 && socket.readyState === 1) {
+          socket.send(event.data)
+        }
+      })
+      mediaRecorder.start(1000)
+    }
+
+    socket.onmessage = (message) => {
+      const received = JSON.parse(message.data);
+      const transcript = received.channel.alternatives[0].transcript
+      if (transcript && received.is_final) {
+        setWords([transcript]);
+        console.log(transcript);
+      }
+    }
+
+    socket.onclose = () => {
+      console.log({ event: 'onclose' })
+    }
+
+    socket.onerror = (error) => {
+      console.log({ event: 'onerror', error })
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {words.map(function(word, i) {
+        return (<div key={i}>{word}</div>)
+      })}
     </div>
-  );
+  )
 }
-
-export default App;
